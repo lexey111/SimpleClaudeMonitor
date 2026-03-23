a013c321db0bb2511e3817b12a999a7adf1f2995
Oleksii Koshkin <lexey111@gmail.com>
2026-03-23 13:21:16 +0100

    Initial Commit

---
+++ SimpleClaudeMonitor/SimpleClaudeMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClaudeMonitorApp.swift
+//  SimpleClaudeMonitor
+//
+//  Created by Oleksii Koshkin on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClaudeMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
